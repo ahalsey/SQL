464facaef7fcb7d0dc5a21bd55cb88d284c90b3c
ahalsey <xandros@linux.com>
2019-04-14 16:35:53 -0600

    Update weeklytickets.sql

--- weeklytickets.sql
+++ weeklytickets.sql
@@ -1,3 +1,6 @@
+/* this SQL query finds the number of Zendesk tickets per week (starting with the week of 2018-01-08)
+that are in groups Engineering, Partner Support, Product Support, Application Support. */
+
 SELECT DATE_ADD("2018-01-08", INTERVAL round(DATEDIFF(request_date, "2018-01-08")/7, 0) week) as weekOf
 , COUNT(ticket_id) AS ticketCount 
 FROM zendesk z 
@@ -6,6 +9,3 @@ AND z.request_date > "2018-01-08"
 AND zgroup IN ("Engineering","Partner Support","Product Support","Application Support") 
 GROUP BY weekOf 
 ORDER BY weekOf ASC;
-
-/* this SQL query finds the number of Zendesk tickets per week (starting with the week of 2018-01-08)
-that are in groups Engineering, Partner Support, Product Support, Application Support. */
